265be084ebd1dbe27d522dedb9dd50fa94ec42ea
danil maikovskiy <maikovsky.danil@gmail.com>
2020-06-19 21:19:08 +0300

    fetch delay is 300

--- src/js/settings.js
+++ src/js/settings.js
@@ -10,4 +10,4 @@ export const INPUT_NODE = document.querySelector('#input');
 export const QUERY_URL = 'https://api.github.com/search/repositories?q=';
 export const SORT_SETTINGS = '&sort=name+stars&order=desc';
 
-export const QUERY_DELAY = 1000;
+export const QUERY_DELAY = 300;
