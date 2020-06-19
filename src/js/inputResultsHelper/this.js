62d85b4f7b98a83dc37412c02c7792846753a984
danil maikovskiy <maikovsky.danil@gmail.com>
2020-06-19 20:50:06 +0300

    refactoring

--- src/js/inputResultsHelper/this.js
+++ src/js/inputResultsHelper/this.js
@@ -1,5 +1,5 @@
 import { addMethod } from './add/this.js';
 import { addEventListenersMethod } from './addEventListeners/this.js';
 
-export const add = addMethod;
+export const add               = addMethod;
 export const addEventListeners = addEventListenersMethod;
