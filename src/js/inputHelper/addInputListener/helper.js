25613773efb9af194124676d4befd391d22d4303
danil maikovskiy <maikovsky.danil@gmail.com>
2020-06-19 20:54:34 +0300

    refactoring

--- src/js/inputHelper/addInputListener/helper.js
+++ src/js/inputHelper/addInputListener/helper.js
@@ -1,10 +1,8 @@
 import { QUERY_URL, SORT_SETTINGS, QUERY_DELAY } from '../../settings.js';
 import { debounce } from '../../helper.js';
 
-const createInputURL = query => new URL(QUERY_URL + query + SORT_SETTINGS);
-
 export const getJsonItems = debounce((inputResultsObj, query) => {
-  fetch(createInputURL(query))
+  fetch(new URL(QUERY_URL + query + SORT_SETTINGS))
     .then(response => response.json())
     .then(json => inputResultsObj.setData(json.items))
     .catch(() => inputResultsObj.clear());
