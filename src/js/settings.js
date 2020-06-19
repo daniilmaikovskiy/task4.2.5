ae55a93202b2fd99df60891ec0a0591d291e8ca9
danil maikovskiy <maikovsky.danil@gmail.com>
2020-06-19 21:32:13 +0300

    delete js

--- src/js/settings.js
+++
@@ -1,6 +0,0 @@
-const REPO_FRAGMENT = document.querySelector('#repo-template').content;
-
-export const REPO         = REPO_FRAGMENT.querySelector('.repo');
-export const REPO_WRAPPER = document.querySelector('#repo-wrapper');
-
-export const INPUT_RESULTS_HTML_ARRAY = Array.from(document.querySelectorAll('.input-result'));
